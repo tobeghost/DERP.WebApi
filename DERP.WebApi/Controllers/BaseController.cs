6dbb4dcabcf214430a595b555f89e9d165a20ef1
İsmail AKTI <mikroajans2005@hotmail.com>
2022-12-03 10:13:09 +0300

    created base controller

---
+++ DERP.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DERP.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public abstract class BaseController : ControllerBase
+{
+    public IEnumerable<T> FindServices<T>()
+    {
+        return HttpContext.RequestServices.GetServices<T>();
+    }
+
+    public T FindService<T>()
+    {
+        return HttpContext.RequestServices.GetService<T>();
+    }
+
+    protected virtual IActionResult InvokeHttp404()
+    {
+        Response.StatusCode = 404;
+        return new EmptyResult();
+    }
+
+    public override ForbidResult Forbid()
+    {
+        return new ForbidResult(JwtBearerDefaults.AuthenticationScheme);
+    }
+}
--- DERP.WebApi/Controllers/LoginController.cs
+++ DERP.WebApi/Controllers/LoginController.cs
@@ -5,63 +5,68 @@ using DERP.Services.Abstract;
 using DERP.WebApi.Domain.Dtos.Customer;
 using DERP.WebApi.Infrastructure.Configuration;
 using DERP.WebApi.Jwt;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 
-namespace DERP.WebApi.Controllers
+namespace DERP.WebApi.Controllers;
+
+[ControllerName("login")]
+public class LoginController : BaseController
 {
-    [ApiController]
-    [Route("login")]
-    public class LoginController : ControllerBase
+    private readonly ICustomerService _customerService;
+    private readonly JwtTokenConfig _jwtTokenConfig;
+
+    public LoginController(ICustomerService customerService,
+        JwtTokenConfig jwtTokenConfig)
     {
-        private readonly ICustomerService _customerService;
-        private readonly JwtTokenConfig _jwtTokenConfig;
-        
-        public LoginController(ICustomerService customerService,
-            JwtTokenConfig jwtTokenConfig)
+        _customerService = customerService;
+        _jwtTokenConfig = jwtTokenConfig;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("sign-in")]
+    [ProducesResponseType(typeof(SignInResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> SignIn(SignInRequest signInRequest)
+    {
+        var result = await _customerService.ValidateCustomer(signInRequest.Username, signInRequest.Password);
+        if (result == "Successful")
         {
-            _customerService = customerService;
-            _jwtTokenConfig = jwtTokenConfig;
+            var claims = new Dictionary<string, string>();
+            claims.Add("Username", signInRequest.Username);
+
+            var token = new JwtTokenBuilder();
+            token.AddSecurityKey(JwtSecurityKey.Create(_jwtTokenConfig.SecretKey));
+            token.AddClaims(claims);
+            token.AddExpiry(_jwtTokenConfig.ExpiryInMinutes);
+
+            if (_jwtTokenConfig.ValidateIssuer)
+                token.AddIssuer(_jwtTokenConfig.ValidIssuer);
+
+            if (_jwtTokenConfig.ValidateAudience)
+                token.AddAudience(_jwtTokenConfig.ValidAudience);
+
+            var jwt = token.Build();
+
+            var re = new SignInResponse();
+            re.Username = signInRequest.Username;
+            re.Token = jwt.Value;
+            re.ExpiryInMinutes = _jwtTokenConfig.ExpiryInMinutes;
+
+            return Ok(re);
         }
 
-        [HttpPost("sign-in")]
-        [ProducesResponseType(typeof(SignInResponse), StatusCodes.Status200OK)]
-        public async Task<IActionResult> SignIn(SignInRequest signInRequest)
+        if (result == "CustomerNotExist")
         {
-            var result = await _customerService.ValidateCustomer(signInRequest.Username, signInRequest.Password);
-            if (result == "Successful")
-            {
-                var claims = new Dictionary<string, string>();
-                claims.Add("Username", signInRequest.Username);
-
-                var token = new JwtTokenBuilder();
-                token.AddSecurityKey(JwtSecurityKey.Create(_jwtTokenConfig.SecretKey));
-                token.AddClaims(claims);
-                token.AddExpiry(_jwtTokenConfig.ExpiryInMinutes);
-                
-                if (_jwtTokenConfig.ValidateIssuer)
-                    token.AddIssuer(_jwtTokenConfig.ValidIssuer);
-                
-                if (_jwtTokenConfig.ValidateAudience)
-                    token.AddAudience(_jwtTokenConfig.ValidAudience);
-
-                var jwt = token.Build();
-
-                var re = new SignInResponse();
-                re.Username = signInRequest.Username;
-                re.Token = jwt.Value;
-                re.ExpiryInMinutes = _jwtTokenConfig.ExpiryInMinutes;
-
-                return Ok(re);
-            }
-            
-            return BadRequest();
+            return NotFound();
         }
-        
-        [HttpPost("sign-out")]
-        public async Task<IActionResult> SignOut()
-        {
-            await Task.Delay(1000);
-            return Ok();
-        }
+
+        return BadRequest();
+    }
+
+    [HttpPost("sign-out")]
+    public async Task<IActionResult> SignOut()
+    {
+        await Task.Delay(1000);
+        return Ok();
     }
 }
